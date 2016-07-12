a4d34344f1bef46050634ef1e1b060c407788a67
ched <christian.eder@zuehlke.com>
2016-07-12 12:00:02 +0200

    added another npm run script for coverage analysis in order to have a non instrumented codebase for debugging unit tests with "npm run test", and running coverage analysis with "npm run test-coverage"

---
+++ Frontend/test/unit/karma.test.conf.js
@@ -0,0 +1,8 @@
+﻿var karmaConfig = require('./karma.common.conf.js');
+
+module.exports = function (config) {  
+    var measureCoverage = false;
+    var testWebpackConfig = require('./webpack.test.config.js')(measureCoverage);
+    karmaConfig.webpack = testWebpackConfig;
+    config.set(karmaConfig);
+}
--- Frontend/test/unit/webpack.test.config.js
+++ Frontend/test/unit/webpack.test.config.js
@@ -1,30 +1,32 @@
-﻿module.exports = {
-    resolve: {
-        root: __dirname,
-        extensions: ['', '.ts', '.js', '.json']
-    },
-    resolveLoader: {
-        modulesDirectories: ["node_modules"]
-    },
-    devtool: "inline-source-map",
-    module: {
-        loaders: [
-            { test: /\.html$/, loader: 'raw' },
-            { test: /\.ts$/, loader: 'ts' },
-            { test: /\.css$/, loaders: ['css-to-string-loader', 'css-loader'] },
-            {
+﻿module.exports = function (measureCoverage) {
+    return {
+        resolve: {
+            root: __dirname,
+            extensions: ['', '.ts', '.js', '.json']
+        },
+        resolveLoader: {
+            modulesDirectories: ["node_modules"]
+        },
+        devtool: "inline-source-map",
+        module: {
+            loaders: [
+                { test: /\.html$/, loader: 'raw' },
+                { test: /\.ts$/, loader: 'ts' },
+                { test: /\.css$/, loaders: ['css-to-string-loader', 'css-loader'] },
+                {
                     test: /\.png$/, loader: "file", query: {
                         emitFile: true,
-                                name: "../wwwroot/assets/images/[name].[ext]"
-                                }
-            }
-        ],
-        postLoaders: [
-          {
-              test: /^((?!\.spec\.ts).)*.ts$/,
-              exclude: /(node_modules|bower_components)/,
-              loader: 'istanbul-instrumenter'
-          }
-        ]
+                        name: "assets/images/[name].[ext]"
+                    }
+                }
+            ],
+            postLoaders: measureCoverage ? [
+              {
+                  test: /^((?!\.spec\.ts).)*.ts$/,
+                  exclude: /(node_modules|bower_components)/,
+                  loader: 'istanbul-instrumenter'
+              }
+            ] : []
+        }
     }
 };
